37ac721f7fd99a79e50605ec57c195d2522c3685
Tomer <ToMeRhh@users.noreply.github.com>
2015-11-19 16:01:04 +0200

    Create pimp.js
    add 'javascript:' at the beginning to save it as a bookmark, or add it to  Tampermonkey

---
+++ pimp.js
@@ -0,0 +1,15 @@
+(function() {
+    imgs = document.getElementsByTagName("img");
+    for (i = 0; i < imgs.length; i++) {
+        if (imgs[i].getAttribute("src") != "Images/block32.png") continue;
+        str = imgs[i].parentNode.getAttribute("onmouseover");
+        str = str.substring(str.indexOf("$('#") + 4, str.indexOf("').show"));
+        str = str.split("_");
+        date = str[0] + "/" + str[1] + "/" + str[2];
+        res = str[3];
+        sevev = str[4];
+        imgs[i].setAttribute("onclick", "goToRes('" + res + "', '" + date + "', '" + sevev + "', 'None')");
+        imgs[i].setAttribute("src", "Images/plus32.png");
+        imgs[i].setAttribute("style", "border: 0px; padding: 0px; width: 15px; height: 15px; background-color: red;");
+    }
+})();
